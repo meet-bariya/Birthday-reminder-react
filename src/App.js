e98443bca28c641e38710587fba2440102bc64e8
MasterMeet <marutimeet@gmail.com>
2021-05-12 00:23:58 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { useState } from 'react';
+import List from './Components/List';
+import data from './Components/data';
 function App() {
+  // eslint-disable-next-line
+  const [people,setPeople] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+     <section className="container">
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+     </section>
+   </main>
   );
 }
 
